19611870375e537cc1411e450cf2857e654b3e78
ingjose95 <104280742+ingjose95@users.noreply.github.com>
2022-07-30 22:20:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ fecha_sistema = time.localtime()
 hora_sistema = fecha_sistema.tm_hour
 
 
-if hora_sistema >= 15:
+if hora_sistema >= 17:
 
     print('Es hora de salir, son las ', str(hora_sistema), ' horas. Feliz noche!')
 
 else:
 
-    horas_restantes = 15 - hora_sistema
+    horas_restantes = 17 - hora_sistema
 
     print('Te faltan: ', horas_restantes, ' para irte a casa. ')
